336c8239f77ae03d15d630be25f6f122d0f53bd4
Matthaeus Mueller <matthaeus.mueller@flagbit.de>
2012-02-07 14:53:29 +0100

    [+TASK] - implemented feuser authentication

--- src/adapter/typo3/ext/fbsso/sso/verify.php
+++ src/adapter/typo3/ext/fbsso/sso/verify.php
@@ -2,7 +2,7 @@
 // Exit, if script is called directly (must be included via eID in index_ts.php)
 if (!defined ('PATH_typo3conf')) die ('Could not access this script directly!');
 
-require_once(PATH_tslib.'class.tslib_pibase.php');
+require_once(PATH_t3lib.'class.t3lib_userauth.php');
 
 /* ToDo: Implement ZendLoader */
 require_once(PATH_t3lib.'Flagbit/Sso.php');
@@ -13,26 +13,38 @@ require_once(PATH_t3lib.'Flagbit/Sso/Storage/Abstract.php');
 require_once(PATH_t3lib.'Flagbit/Sso/Storage/Json.php');
 
 
-class ssoAdapter extends tslib_pibase
+class ssoAdapter extends t3lib_userauth
 {
   protected $_data = NULL;
   protected $_sso = NULL;
 
   public function __construct()
   {
+    $GLOBALS['TSFE']->fe_user = tslib_eidtools::initFeUser();
+    tslib_eidtools::connectDB();
     $this->_data = t3lib_div::_GP('data');
     $this->_sso = new Flagbit_Sso;
   }
 
+  protected function _getUserByEmail($email)
+  {
+	return $GLOBALS["TYPO3_DB"]->sql_fetch_assoc( $GLOBALS["TYPO3_DB"]->exec_SELECTquery( "*", "fe_users", "email='" . $email . "'" ) );
+  }
   public function loginUser()
   {
-   if( $this->_sso->loadContainer($this->_data)->verify( $this->_getPublicKey() ) === TRUE ) {
-    	// login successful
-		echo "login successful";
-   } else {
-   		// login failed
-		echo "something failed"; 
-   }
+	if( $this->_sso->loadContainer($this->_data)->verify( $this->_getPublicKey() ) === TRUE )
+	{
+		$user = $this->_getUserByEmail( $this->_sso->getContainer()->getValue('container/email') );
+		if( $user )
+		{
+			$GLOBALS['TSFE']->fe_user->createUserSession($user);
+			t3lib_utility_Http::redirect();
+		} else {
+			// create user
+		}		
+	} else {
+		t3lib_utility_Http::redirect();
+	}
   }
 
   protected function _getPublicKey()
