26b7c40f0dcde7a07f31362ccf575898db9d8d06
Matthaeus Mueller <matthaeus.mueller@flagbit.de>
2012-03-26 10:53:05 +0200

    [+TASK]: Removed library from adapter

--- src/library/Flagbit/Sso/Signature/Interface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface Flagbit_Sso_Signature_Interface
-{
-	public function createSign($data, $privateKey);
-	public function verifySign($data, $signature, $publicKey);
-}
--- src/library/Flagbit/Sso/Storage/Interface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface Flagbit_Sso_Storage_Interface
-{
-	public function getValue($path);
-	public function setValue($path, $value);
-}
